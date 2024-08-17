3ecf88e51a1e2063c1a67cf1f2584e58e2aa12bc
Satsuki Akiba <anosatsuk124@gmail.com>
2024-08-17 20:12:46 +0900

    Completed tetris loop

--- src/Korobeiniki/score.py
+++ src/Korobeiniki/score.py
@@ -13,7 +13,7 @@ from fluid import FLUID
 SCALE = pitch.Scale.from_pitches([60, 62, 64-1, 65, 67, 69-1, 71-1, 72])
 
 
-TEMPO = 140
+TEMPO = 100
 
 # ####START SCORES####
 
@@ -30,44 +30,92 @@ def set_insts():
 
 
 def melody(clock: sc.Clock, track: sc.ScampInstrument):
-    props = None
+    props1 = None
     vol = 0.3
 
     scale = copy.deepcopy(SCALE)
-    scale = scale.transpose(12)
+    # scale = scale.transpose(12)
 
-    dur = [1, 1/2, 1/2]
+    dur1 = [1, 1/2, 1/2]
 
-    melody = [
+    melody1 = [
         [4, 1, 2],
         [3, 2, 1],
         [0, 0, 2],
         [4, 3, 2],
+        [1, 1, 2]
     ]
+    props2 = (sc.StartSlur(), sc.StopSlur(), None)
 
-    for i, m in enumerate(melody):
-        u.play_score(track, m, dur, scale, vol, props=props)
+    dur2 = [1, 1]
 
-    u.play_score(track, [1], [1], scale, vol, props=[sc.StartSlur()])
-    u.play_score(track, [1], [1/2], scale, vol, props=[sc.StopSlur()])
-    u.play_score(track, [2], [1/2], scale, vol)
-
-    dur = [1, 1]
-
-    melody = [
+    melody2 = [
         [3, 4],
         [2, 0],
         [0, None]
     ]
 
-    for i, m in enumerate(melody):
-        u.play_score(track, m, dur, scale, vol, props=props)
+    melody4 = [
+        [7, 6, 5],
+        [4, 4, 2],
+    ]
 
-    dur = [1/2, 1/2, 1/2, 1/2]
+    dur4 = dur1
 
-    u.play_score(track, [None, 3, 3, 5], dur, scale, vol, props=props)
+    def base1():
 
-    # u.play_score(track)
+        for i, m in enumerate(melody1):
+            u.play_score(track, m, dur1, scale, vol, props=tuple(
+                [None] * (len(melody1)-1)) + props2)
+
+        for i, m in enumerate(melody2):
+            u.play_score(track, m, dur2, scale, vol, props=props1)
+
+    def change():
+        dur = [1/2, 1/2, 1/2, 1/2]
+
+        melody = [None, 3, 3, 5]
+
+        u.play_score(track, melody, dur, scale, vol,
+                     props=(None, sc.StartSlur(), sc.StopSlur(), None))
+
+    def base2():
+        for i, m in enumerate(melody4):
+            u.play_score(track, m, dur4, scale, vol,
+                         props=tuple([None]*3)+props2)
+
+        for i, m in enumerate(melody1[3:]):
+            u.play_score(track, m, dur1, scale, vol, props=props1)
+
+        for i, m in enumerate(melody2):
+            u.play_score(track, m, dur2, scale, vol, props=props1)
+
+    tempo = TEMPO
+
+    for i in range(3):
+        for j in range(2):
+            print(clock.tempo)
+
+            base1()
+            change()
+            base2()
+
+            change()
+            base2()
+
+            scale.transpose(1)
+
+            change()
+            base2()
+
+            tempo += 5 + j
+            clock.tempo = tempo
+
+            scale.transpose(1)
+
+        scale.transpose(2)
+        tempo += 10 + i
+        clock.tempo = tempo
 
     u.rest(2)
 
@@ -80,6 +128,6 @@ def scores(session: sc.Session):
         "piano", out, start_channel=0
     )
 
-    session.fork(melody, args=[piano1])
+    session.fork(melody, args=[piano1], initial_tempo=TEMPO)
 
 # ####END SCORES####
